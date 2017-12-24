ab954dea305d15a7dd9ccb9c2736c99e7ff0d7f8
Erik Baliov <vfxbaliof@gmail.com>
2017-12-24 23:30:40 +0200

    Showing the Android version.

--- app/src/main/java/com/example/baliofvfx/phonestatus/StatsFragment.java
+++ app/src/main/java/com/example/baliofvfx/phonestatus/StatsFragment.java
@@ -1,14 +1,26 @@
 package com.example.baliofvfx.phonestatus;
 
+import android.os.Build;
 import android.os.Bundle;
+import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 
 public class StatsFragment extends Fragment {
 
+
+    private TextView androidVersionTextView;
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        androidVersionTextView = (TextView)view.findViewById(R.id.androidVersionTextView);
+        androidVersionTextView.setText("Android SDK: " + Build.VERSION.SDK_INT + " " + Build.VERSION.RELEASE);
+    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
