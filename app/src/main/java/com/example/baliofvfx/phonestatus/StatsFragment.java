b92e7197bdf9a68ac462d374da31ea6d5984ed36
Erik Baliov <vfxbaliof@gmail.com>
2017-12-27 17:06:57 +0200

    Showing phone uptime status.

--- app/src/main/java/com/example/baliofvfx/phonestatus/StatsFragment.java
+++ app/src/main/java/com/example/baliofvfx/phonestatus/StatsFragment.java
@@ -4,6 +4,7 @@ import android.net.wifi.WifiInfo;
 import android.os.BatteryManager;
 import android.os.Build;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
@@ -17,14 +18,27 @@ public class StatsFragment extends Fragment {
 
 
     private TextView androidVersionTextView;
+    private TextView systemUpTimeTextView;
 
+    int secondsUpTime = 0;
+    int minutesUpTime = 0;
+    int hoursUpTime = 0;
+    int daysUpTime = 0;
+
+    String uptimeArray[] = new String[4];
+
+    @Override
+    public void onStart() {
+        super.onStart();
+        showUptime(systemUpTimeTextView);
+        androidVersionTextView.setText("Android SDK: " + Build.VERSION.SDK_INT + " " + Build.VERSION.RELEASE);
+    }
 
     @Override
     public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
         super.onViewCreated(view, savedInstanceState);
         androidVersionTextView = (TextView)view.findViewById(R.id.androidVersionTextView);
-        androidVersionTextView.setText("Android SDK: " + Build.VERSION.SDK_INT + " " + Build.VERSION.RELEASE);
-
+        systemUpTimeTextView = (TextView)view.findViewById(R.id.systemUpTimeTextView);
     }
 
     @Override
@@ -32,5 +46,49 @@ public class StatsFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_stats, container, false);
+    }
+
+
+    private void showUptime(TextView textview){
+        secondsUpTime = (int)(SystemClock.elapsedRealtime() / ((1000)) % 60);
+        minutesUpTime = (int)(SystemClock.elapsedRealtime() / ((1000 * 60)) % 60);
+        hoursUpTime = (int)(SystemClock.elapsedRealtime() / ((1000 * 60 * 60)) % 24);
+        daysUpTime = (int)(SystemClock.elapsedRealtime() / (1000*60*60*24));
+
+
+        hoursToString(daysUpTime, hoursUpTime, minutesUpTime, secondsUpTime); // Formats the uptime array[]
+        textview.setText("System uptime: " +  uptimeArray[0] + ":" + uptimeArray[1] + ":" + uptimeArray[2] + ":" + uptimeArray[3]);
+    }
+
+    private void hoursToString(int days, int hours, int minutes, int seconds){
+        if(days <= 9){
+            uptimeArray[0] = "0" + Integer.toString(days);
+        }
+        else{
+            uptimeArray[0] = Integer.toString(days);
+        }
+
+        if(hours <= 9){
+            uptimeArray[1] = "0" + Integer.toString(hours);
+        }
+        else{
+            uptimeArray[1] = Integer.toString(hours);
+        }
+
+        if(minutes <= 9){
+            uptimeArray[2] = "0" + Integer.toString(minutes);
+        }
+        else{
+            uptimeArray[2] = Integer.toString(minutes);
+        }
+
+        if(seconds <= 9){
+            uptimeArray[3] = "0" + Integer.toString(seconds);
+        }
+        else{
+            uptimeArray[3] = Integer.toString(seconds);
+        }
+
+
     }
 }
