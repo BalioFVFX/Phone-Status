e956048b655e9625b6d2c55e2b3bc4442b09d1ae
Erik Baliov <vfxbaliof@gmail.com>
2017-12-30 16:07:35 +0200

    Drawing a battery for better representation of the battery level

--- app/src/main/java/com/example/baliofvfx/phonestatus/BatteryLevelFragment.java
+++ app/src/main/java/com/example/baliofvfx/phonestatus/BatteryLevelFragment.java
@@ -1,5 +1,9 @@
 package com.example.baliofvfx.phonestatus;
 
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.Color;
+import android.graphics.Paint;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -7,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 
@@ -16,11 +21,26 @@ public class BatteryLevelFragment extends Fragment {
 
     private TextView batteryLevelTextView;
     private BatteryStatus batteryStatus = new BatteryStatus();
+    private ImageView batteryLevelImage;
 
     @Override
     public void onStart() {
         super.onStart();
         RequestManager.sendPost(batteryStatus.batteryLevel(getContext()));
+        drawBatteryLevel((int)batteryStatus.batteryLevel(getContext()),batteryLevelImage);
+    }
+
+    private void drawBatteryLevel(int batteryLevel, ImageView batteryLevelImage){
+        batteryLevel = 100 - batteryLevel;
+
+        Bitmap bitmap = Bitmap.createBitmap(200,300, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
+        paint.setColor(Color.GREEN);
+        canvas.drawRect(0,0,170,300, paint);
+        paint.setColor(Color.GRAY);
+        canvas.drawRect(0,0,170,batteryLevel * 3, paint);
+        batteryLevelImage.setImageBitmap(bitmap);
     }
 
     @Override
@@ -34,6 +54,8 @@ public class BatteryLevelFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_battery_level, container, false);
+        batteryLevelImage = (ImageView)view.findViewById(R.id.batteryLevelImageViewID);
+
 
         final Button button = view.findViewById(R.id.buttonTestId);
         button.setOnClickListener(new View.OnClickListener() {
